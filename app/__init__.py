90c221a4f7ad07e3bcfa09b96da5240588461d4c
Py_TaiYuan_WangXin <wangxin@zhulong.com.cn>
2023-12-11 18:04:36 +0800

    ch7-04 Flask调试工具

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,25 @@
+import os
 from flask import Flask
+from dynaconf import FlaskDynaconf
+
 
 def create_app():
     """Initialize the Flask app instance"""
     app = Flask(__name__)
+    dynaconf = FlaskDynaconf(extensions_list=True)
 
-    # import the routes
-    from . import intro
+    with app.app_context():
 
-    # register the blueprints
-    app.register_blueprint(intro.intro_bp)
+        # Initialize plugins
+        os.environ['ROOT_PATH_FOR_DYNACONF'] = app.root_path
+        dynaconf.init_app(app)
+        app.config["SECRET_KEY"] = bytearray(app.config["SECRET_KEY"], 'UTF-8')
+        # print(app.config["SECRET_KEY"])
 
-    return app
+        # import the routes
+        from . import intro
+
+        # register the blueprints
+        app.register_blueprint(intro.intro_bp)
+
+        return app
