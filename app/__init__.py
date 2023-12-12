5d4c8c5dfd8d47701471aecacc16f26be0ad9355
Py_TaiYuan_WangXin <wangxin@zhulong.com.cn>
2023-12-12 14:22:00 +0800

    添加favicon和网站图标

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 import yaml
-from flask import Flask
+from flask import Flask, send_from_directory
 from pathlib import Path
 from dynaconf import FlaskDynaconf
 import logging
@@ -10,17 +10,29 @@ import logging.config
 def create_app():
     """Initialize the Flask app instance"""
     app = Flask(__name__)
+    # FlaskDynaconf 会自动读取settings.toml和.secrets.toml
     dynaconf = FlaskDynaconf(extensions_list=True)
 
     with app.app_context():
 
+        # # create a route to the favicon.ico file
+        @app.route('/favicon.ico')
+        def favicon():
+            # print('>>>', app.root_path)
+            return send_from_directory(
+                os.path.join(app.root_path, 'static', 'images'),
+                'favicon.ico',
+                mimetype="image/vnd.microsoft.icon"
+            )
+
         # Initialize plugins
         os.environ['ROOT_PATH_FOR_DYNACONF'] = app.root_path
         dynaconf.init_app(app)
+        # Flask debug toolbar 需要这个设置
         app.config["SECRET_KEY"] = bytearray(app.config["SECRET_KEY"], 'UTF-8')
         # print(app.config["SECRET_KEY"])
 
-        _configure_logging(app, dynaconf)  # 设置日志
+        # _configure_logging(app, dynaconf)  # 设置日志
 
         # import the routes
         from . import intro
